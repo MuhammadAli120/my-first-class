c1320a46570816be81a63c4bebbd8b5514bd4822
unknown <malizaheerhassan@gamil.com>
2023-05-05 21:31:50 +0500

    done

---
+++ HTML Javascript/index.js
@@ -0,0 +1,5 @@
+document.getElementById("demo").innerHTML = "HELLO FUTURE DEVELOPER";
+document.getElementById("demo").style.fontSize = "25px";
+document.getElementById("demo").style.color = "red";
+document.getElementById("demo").style.backgroundColor = "yellow";
+document.getElementById("image").src = "wali shah .jpg";
